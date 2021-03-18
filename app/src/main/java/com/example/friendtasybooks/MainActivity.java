96a99aeaa92bf7c4d19a730c4d10544f319910f7
Maximus <s110713063@stu.ntue.edu.tw>
2021-03-19 03:50:27 +0800

    sidebar color & header

--- app/src/main/java/com/example/friendtasybooks/MainActivity.java
+++ app/src/main/java/com/example/friendtasybooks/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_profile, R.id.nav_trade, R.id.nav_badge, R.id.nav_friends)
+                R.id.nav_home,  R.id.nav_profile, R.id.nav_mybook, R.id.nav_badge, R.id.nav_friends)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
