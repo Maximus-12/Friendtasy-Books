22fdbf2c2d497d92d6c5d25680fc040a3b50d10c
Maximus <s110713063@stu.ntue.edu.tw>
2021-03-27 03:03:41 +0800

    add login activity

--- app/src/main/java/com/example/friendtasybooks/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/friendtasybooks/ui/home/HomeFragment.java
@@ -1,4 +1,4 @@
-package com.example.friendtasybooks.ui.home;
+/*package com.example.friendtasybooks.ui.home;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -31,5 +31,33 @@ public class HomeFragment extends Fragment {
             }
         });
         return root;
+    }
+}*/
+package com.example.friendtasybooks.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.friendtasybooks.R;
+import com.example.friendtasybooks.ui.slideshow.SlideshowViewModel;
+
+public class HomeFragment extends Fragment {
+
+
+    //private ProfileViewModel profileViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
